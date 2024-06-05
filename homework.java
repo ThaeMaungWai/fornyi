573b64288bf25dca435ad95c88297eb3f82e1989
Win Htet Aung <winhtataung11@gmail.com>
2024-06-05 21:46:10 +0630

    Push homework

---
+++ homework.java
@@ -0,0 +1,25 @@
+package winhtetaung;
+
+public class homework {
+
+	public static void main(String[] args) {
+		int result = 7 * 8 + 2 - 5 / 1;
+		System.out.println(result);
+		
+		int X = 20 + 10;
+		int Y = 30 / 5;
+		int C = X * Y;
+		System.out.println(C);
+		
+		int A = 100;
+		System.out.println(A);
+		
+		    boolean isJavaFun = true;
+	        boolean isFishTasty = false;
+	        System.out.println("Is Java fun? " + isJavaFun);
+	        System.out.println("Is fish tasty? " + isFishTasty);
+	    
+		
+    }
+}
+      
